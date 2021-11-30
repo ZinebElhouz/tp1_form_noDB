4aa5c0311d41754bed5a4adc989dcedc33b9acbc
zineb <zineb@LAPTOP-5FQRSO2V>
2021-11-30 13:50:22 +0100

    Form1.cs

---
+++ Etudiant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1_form
+{
+    class Etudiant
+    {
+        public string cin { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+
+    }
+}
